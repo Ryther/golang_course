e7c1f5d3813b1078e88d2cfac2c822042b84210b
Edoardo Zanoni <edoardo.zanoni@infocert.it>
2024-04-03 15:02:55 +0200

    feat(lesson-053): added files for lesson 053

---
+++ 053-hands_on_exercise_15/go.mod
@@ -0,0 +1,3 @@
+module github.com/Ryther/golang_course/lesson_053
+
+go 1.22.1
---
+++ 053-hands_on_exercise_15/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x string = "Hello from the other side"
+	fmt.Println(x)
+}
